0d3147a86ea46f0fee546e589c059a0477d14138
judetsuki <Luka9609@gmail.com>
2025-05-10 21:24:52 +0300

    test commit 5

--- frontend/src/utils/server.js
+++ frontend/src/utils/server.js
@@ -1,7 +1,15 @@
 import express from 'express';
-
+import cors from "cors";
 const app = express();
 app.use(express.json());
+
+app.use(cors());
+app.use(cors({ origin: 'https://your-subdomain.loca.lt/' }));
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type']
+}));
 
 const FLASK_API = process.env.FLASK_API || 'https://task-tracker-daniel.loca.lt';
 
